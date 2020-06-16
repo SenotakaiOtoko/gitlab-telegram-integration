8f134e28165021716dbf3a550df6c48b4bd9fa2a
Rybakov Viktor <V.Rybakov@hostco.ru>
2020-06-16 19:23:03 +0500

    Добавить работу с базой

--- src/main/java/ru/hostco/dvs/gitlabtelegramintegration/service/Syncer.java
+++ src/main/java/ru/hostco/dvs/gitlabtelegramintegration/service/Syncer.java
@@ -1,6 +1,7 @@
 package ru.hostco.dvs.gitlabtelegramintegration.service;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
@@ -8,6 +9,10 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.hostco.dvs.gitlabtelegramintegration.config.ApplicationConfiguration;
@@ -17,6 +22,10 @@ import ru.hostco.dvs.gitlabtelegramintegration.dto.telegram.Chat;
 import ru.hostco.dvs.gitlabtelegramintegration.dto.telegram.Message;
 import ru.hostco.dvs.gitlabtelegramintegration.dto.telegram.Response;
 import ru.hostco.dvs.gitlabtelegramintegration.dto.telegram.Update;
+import ru.hostco.dvs.gitlabtelegramintegration.jpa.models.GitlabTelegramUserMapping;
+import ru.hostco.dvs.gitlabtelegramintegration.jpa.models.UpdateOffset;
+import ru.hostco.dvs.gitlabtelegramintegration.jpa.repositories.GitlabTelegramUserMappingRepository;
+import ru.hostco.dvs.gitlabtelegramintegration.jpa.repositories.UpdateOffsetRepository;
 
 @Slf4j
 @Service
@@ -26,31 +35,60 @@ public class Syncer {
   private final GitLabClient gitLabClient;
   private final ApplicationConfiguration applicationConfiguration;
   private final TelegramBotClient telegramBotClient;
-
-  //@Scheduled(fixedDelay = 6 * 1000)
+  private final GitlabTelegramUserMappingRepository gitlabTelegramUserMappingRepository;
+  private final UpdateOffsetRepository updateOffsetRepository;
 
   @Scheduled(fixedDelay = 6 * 1000)
-  public void updateMergeRequests() {
-    final String me = telegramBotClient.getMe(applicationConfiguration.getTelegramBotToken());
+  public void updateUsers() {
 
+    final Long updateOffset = updateOffsetRepository
+        .findAllOrderByDateInsertDesc(PageRequest.of(0, 1, Sort.by("dateInsert").descending()))
+        .stream().map(UpdateOffset::getUpdateId).filter(Objects::nonNull).map(u -> u + 1)
+        .findFirst().orElse(null);
+    /**
+     * Сообщения от телеграмма
+     */
     final Response<Collection<Update>> updates = telegramBotClient
-        .getUpdates(applicationConfiguration.getTelegramBotToken());
+        .getUpdates(applicationConfiguration.getTelegramBotToken(), updateOffset);
 
+    if (updates.getResult().isEmpty()) {
+      return;
+    }
+
+    final Long lastGotUpdateId = updates.getResult().stream().map(Update::getUpdateId)
+        .filter(Objects::nonNull).sorted(Comparator.reverseOrder()).findFirst().get();
 
     final List<Message> telegramToGitlabUsernames = updates.getResult().stream()
         .map(Update::getMessage).filter(Objects::nonNull)
         .filter(m -> m.getText().matches("/gitlabusername \\w+")).collect(Collectors.toList());
 
-    /**
-     * TODO: Сохранять эти данные в БД
-     */
-    final HashMap<String, Chat> gitlabToTelegramUser = new HashMap<>();
+    telegramToGitlabUsernames.stream().forEach(t -> {
+      final Page<GitlabTelegramUserMapping> databaseUsernameMapping = gitlabTelegramUserMappingRepository
+          .findAllByTelegramUsernameEqualsIgnoreCase(t.getFrom().getUsername(), Pageable.unpaged());
+      gitlabTelegramUserMappingRepository.deleteAll(databaseUsernameMapping);
 
-    telegramToGitlabUsernames.stream().forEach(o -> {
-      final String gitlabUsername = o.getText().replaceAll("/gitlabusername\\s+", "");
-      gitlabToTelegramUser.put(gitlabUsername, o.getChat());
+      final String gitlabUsername = t.getText().replaceAll("/gitlabusername\\s+", "");
+
+      final GitlabTelegramUserMapping newMapping = GitlabTelegramUserMapping
+          .builder()
+          .telegramUsername(t.getFrom().getUsername())
+          .telegramChatId(t.getChat().getId())
+          .telegramFirstName(t.getFrom().getFirstName())
+          .telegramLastName(t.getFrom().getLastName())
+          .gitlabUsername(gitlabUsername)
+          .build();
+
+      gitlabTelegramUserMappingRepository.save(newMapping);
     });
 
+    final UpdateOffset newUpdateOffset = UpdateOffset.builder().updateId(lastGotUpdateId).build();
+    updateOffsetRepository.save(newUpdateOffset);
+
+    log.info("telegram part completed");
+  }
+
+  @Scheduled(fixedDelay = 6 * 1000, initialDelay = 1 * 1000)
+  public void updateMergeRequests() {
     final String gitlabPrivateToken = applicationConfiguration.getGitlabPrivateToken();
 
 
@@ -90,18 +128,22 @@ public class Syncer {
           /**
            * Отправляем уведомление и ссылку на merge request
            */
-          final Chat assigneeChat = gitlabToTelegramUser.get(newAssignee.getUsername());
-          if (assigneeChat != null) {
+          final Page<GitlabTelegramUserMapping> notifyUsers = gitlabTelegramUserMappingRepository
+              .findAllByGitlabUsernameEqualsIgnoreCase(newAssignee.getUsername(),
+                  Pageable.unpaged());
+
+
+          notifyUsers.forEach(u -> {
             final StringBuilder text = new StringBuilder()
-                .append(assigneeChat.getUsername())
+                .append(u.getTelegramUsername())
                 .append(", на вас назначен МР: \n")
                 .append(m.getWebUrl());
 
-
             telegramBotClient
-                .sendMessage(applicationConfiguration.getTelegramBotToken(), assigneeChat.getId(),
+                .sendMessage(applicationConfiguration.getTelegramBotToken(), u.getTelegramChatId(),
                     text.toString());
-          }
+
+          });
         });
 
     log.info("mergeRequests");
